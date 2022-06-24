f0f566acb9e2c97c0bc8f87f3d3903630ae19ea9
Mohit Goyal <mohit.g.mg1996@gmail.com>
2022-06-24 18:05:24 +0530

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api/v1');
+  const config = new DocumentBuilder()
+    .setTitle('NestJS example')
+    .setDescription('The NextJS API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/dto/auth.ts
+++ src/modules/auth/dto/auth.ts
@@ -1,16 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
 
 export class AuthDTO {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'example@email.com',
+    description: "User's email.",
+  })
   email: string;
 
   @IsNotEmpty()
   @MinLength(6)
   @MaxLength(32)
+  @ApiProperty({ example: '121212', description: "User's password." })
   password: string;
 
+  @ApiProperty({
+    example: 1234567890,
+    description: "User's Phone Number",
+    required: false,
+  })
   phone: string;
 
+  @ApiProperty({
+    example: 'Alex',
+    description: "User's Name.",
+    required: false,
+  })
   name: string;
 }
